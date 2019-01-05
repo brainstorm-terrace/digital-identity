5423892cdd6cd451db3abb3cf20b538adaf5261f
madhukumarm <madhu.ecbe@gmail.com>
2019-01-05 10:39:20 +0530

    get request implemented for user-profile app

--- app/middleware/digital_identity/user_profile/urls.py
+++ app/middleware/digital_identity/user_profile/urls.py
@@ -1,14 +1,8 @@
 from django.conf.urls import url, include
 
-from rest_framework import routers
-
 from . import views
 
-router = routers.DefaultRouter()
 
-router.register('profile', views.UserProfileViewSet)
-router.register('login', views.LoginViewSet, base_name='login')
 urlpatterns = [
-    url(r'', include(router.urls)),
     url(r'user-profile', views.UserProfileView.as_view()),
 ]
--- app/middleware/digital_identity/user_profile/views.py
+++ app/middleware/digital_identity/user_profile/views.py
@@ -4,6 +4,8 @@ from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.core import serializers
+from django.http import HttpResponse, JsonResponse
 
 from . import serializer
 from . import models
@@ -12,38 +14,33 @@ from .serializer import UserProfileSerializer
 
 from dotenv import load_dotenv
 from django.conf import settings
-
+from django.db.models import Q
 import requests
 import json
 import os
 
 # Create your views here.
 
-class UserProfileViewSet(viewsets.ModelViewSet):
-    """Handles creating, updating and deleting profiles"""
-
-    serializer_class = serializer.UserProfileSerializer
-    queryset = models.UserProfile.objects.all()
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (permissions.UpdateOwnProfile,)
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ('name','email',)
-
-
-
-class LoginViewSet(viewsets.ViewSet):
-    """Checks email and password and returns an auth token"""
-
-    serializer_class = AuthTokenSerializer
-
-    def create(self, request):
-        """Use the ObtainAuthToken APIView to validate and create a token"""
-
-        return ObtainAuthToken().post(request)
-
-
 class UserProfileView(APIView):
     """ User Profile APIView. """
+
+    def get(self, request):
+        """ Get list of all users or specified users in the database"""
+        request_data = dict(request.GET)
+
+        if request_data:
+            """Get list of request users"""
+            # print("Request data has some params")
+            # print("request_data",request_data)
+            name_list = request_data.get('name')
+            email_list = request_data.get('email')
+            users_qs = models.UserProfile.objects.filter(Q(name__in=name_list) | Q(email__in=email_list)).values('email', 'name', 'is_active', 'is_superuser', 'is_staff')
+        else:
+            """Get list of all users"""
+            # print("Requested data is empty")
+            users_qs = models.UserProfile.objects.values('email', 'name', 'is_active', 'is_superuser', 'is_staff')
+
+        return JsonResponse(list(users_qs), safe=False)
 
     def post(self, request):
         """ Creates a new user if username and Email didn't exist in the database. """
@@ -60,7 +57,6 @@ class UserProfileView(APIView):
             user_dict.update(user_profile_serializer.data)
             return Response(user_dict, status=status.HTTP_201_CREATED)
         return Response(user_profile_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
     def create_user_id(self, user_data):
         """ Method to call "users-categories/user-detail" api and return user_id.. """
